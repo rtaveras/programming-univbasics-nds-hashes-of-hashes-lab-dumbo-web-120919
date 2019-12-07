142f21cd9891114b6512aeeefcb388be651a9de9
Riaan Taveras <riaantaveras@gmail.com>
2019-12-07 22:09:50 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,5 +32,15 @@
 
 def naming_system
  {label: "Kingdom",
- sub_category: 
+ sub_category: {
+   label: "Phylum"},
+   sub_category:{
+   label: "Class",
+   label: "Order",
+   label: "Family"},
+   sub_category: {
+     label: "Genus",
+     label: "Species"},
+     sub_category: nil
+   }
 end
